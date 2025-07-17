767b671fc3de0ddd68b549200a66c798de685854
Umair Khokhar <umair.ali.khokhar@hotmail.com>
2025-07-17 10:01:07 -0400

    chore: promote repo root to folder Synchrypt and add api/ scaffold

---
+++ api/src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('API server is running'));
+
+app.listen(PORT, () =>
+  console.log(`Server listening on port ${PORT}`)
+);
